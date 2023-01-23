82c9a65d29de6d570a6c79d907e65eae7ce0d5a9
mburakunuvar <mburakunuvar@gmail.com>
2023-01-23 10:32:22 +0100

    updated VPC Subnet-AZ config for us-west-2/Oregon region

--- main-VPC.tf
+++ main-VPC.tf
@@ -107,7 +107,7 @@ resource "aws_eip" "nat_gateway_eip" {
 resource "aws_nat_gateway" "nat_gateway" {
   depends_on    = [aws_subnet.public_subnets]
   allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  subnet_id     = aws_subnet.public_subnets["public_subnet_3"].id
   tags = {
     Name = "demo_nat_gateway"
   }
